265faa67e139e94b04276554afbc51a4000a831e
aiden0921an <nelsonaiden23@gmail.com>
2024-08-13 13:31:13 -0500

    cleaning up routing

--- controller/api/index.js
+++ controller/api/index.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
 const profileRoutes= require('./profile-routes')
+const searchResultsRoutes= require('./profile-routes')
 
 // When a request is made to the /users or /projects path, it will be directed to the index.js in the /users or /projects folder.
 router.use('/users', userRoutes);
