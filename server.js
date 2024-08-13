c4616cb84a7758b669f7414da3ae3896f868b1d6
Ben Merchant <benmerch@gmail.com>
2024-08-13 11:41:00 -0500

    Created the models and seed info. Successful seed test completed.

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const session = require('express-session');
-const routes = require('./controllers');
+const routes = require('./controller');
 const exphbs = require('express-handlebars'); 
 const hbs = exphbs.create({});
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-// const helpers = require('./utils/helpers'); 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Keep this sync setting set to false and just use the seed file to update the database 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
-=======
 const sess = {
   secret: 'Super secret secret',
   cookie: {
@@ -31,17 +24,17 @@ const sess = {
   }),
 };
 
-// const hbs = exphbs.create({ helpers });
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
 app.use(express.static('public'));
 app.use(session(sess));
 
+app.use(routes);
+
+// Keep this sync setting set to false and just use the seed file to update the database 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
