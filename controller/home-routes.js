fa4e541f5648da17aeba4406d4646a2d415fcf3a
aiden0921an <nelsonaiden23@gmail.com>
2024-08-14 11:55:08 -0500

    homepage routing refactoring

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
-const { Like, Reviews, User } = require('../models');
+
+const { Likes, Reviews, Users } = require('../models');
 
 
 
@@ -9,7 +10,17 @@ const { Like, Reviews, User } = require('../models');
 
 
 
-//to display a set group of current movies, I could create an array of objects here in to make it be consistent. Also probably not functional yet 
+
+
+
+
+
+
+
+
+//to display a set group of current movies, I could create an array of objects here in to make it be consistent. Also probably not functional yet
+
+
 
 
 router.get('/', async (req, res) => {
@@ -27,77 +38,38 @@ router.get('/', async (req, res) => {
   });
 
 
+  router.get('/reviews/:id', async (req, res) => {
+    try {
+      const dbReviewData = await Reviews.findByPk(req.params.id, {
+        include: [{model: Reviews,
+            attributes: [
+              'id',
+              'title',
+              'author_id',
+              'like_id',
+              'is_public',
+            ],
+          },
+        ],
+      });
+ 
+      const review = dbReviewData.get({ plain: true });
+      res.render('reviews', { review });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
 
 
-// router.get('/', async (req, res) => {
-//     try {
-//       const dbMovieData = await Movie.findAll({});
-//       const movies = dbMovieData.map((movie) =>
-//         movie.get({ plain: true })
-//       );
-//       console.log(movies)
-//       res.render('homepage', {movies});
-//     } catch (err) {
-//       console.log(err);
-//       res.status(500).json(err);
-//     }
-//   });
 
-  //to display a set group of current movies, I could create an array of objects here in to make it be consistent. Also probably not functional yet 
-  // router.get('/movie/:num', async (req,res) => {
-  //   const idx = parseInt(req.params.num) -1;
-  //   console.log(idx);
-  //   const movieToRender= Movie[idx]
-  //   console.log(movieToRender);
-  //   res.render('movie', movieToRender)
-  // });
+
+ 
 
 
 
-// router.get('/', async (req, res) => {
-//     try {
-//       const dbMovieData = await Movie.findAll({});
-//       const movies = dbMovieData.map((movie) =>
-//         movie.get({ plain: true })
-//       );
-//       console.log()
-//       res.render('homepage', {movies});
-//     } catch (err) {
-//       console.log(err);
-//       res.status(500).json(err);
-//     }
-//   });
 
-
-  // router.get('/', async (req, res) => {
-  //   try {
-
-  //     const dbLikesData = await Movie.findAll({});
-  //     const likes = dbLikesData.map((like) =>
-  //       like.get({ plain: true })
-
-  //     const dbReviewData = await Reviews.findAll({
-  //       // include: [
-  //       //   {
-  //       //     model: Reviews,
-  //       //     attributes: ['title', 'description'], //likely should add a description or similar field into the Reviews model
-  //       //   },
-  //       // ],
-  //     });
-  
-  //     const reviews = dbReviewData.map((review) =>
-  //       review.get({ plain: true })
-
-  //     );
-  //     console.log(likes)
-  //     res.render('homepage', {likes});
-  //   } catch (err) {
-  //     console.log(err);
-  //     res.status(500).json(err);
-  //   }
-  // });
-
-  //to display a set group of current movies, I could create an array of objects here in to make it be consistent. Also probably not functional yet 
+  //to display a set group of current movies, I could create an array of objects here in to make it be consistent. Also probably not functional yet
 //   router.get('/movie/:num', async (req,res) => {
 //     const idx = parseInt(req.params.num) -1;
 //     console.log(idx);
@@ -107,13 +79,17 @@ router.get('/', async (req, res) => {
 //   });
 
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-    res.render('login');
-  });
+
+
+// router.get('/login', (req, res) => {
+//     if (req.session.loggedIn) {
+//       res.redirect('/');
+//       return;
+//     }
+//     res.render('login');
+//   });
+
+
 
 
   module.exports = router;
