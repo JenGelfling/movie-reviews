4cda8b7d7598ca544e904919fdef41b21e3ee64c
Ben Merchant <benmerch@gmail.com>
2024-08-18 18:04:02 -0500

    Changed connection.js to try to fix Render issues.

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,53 @@
 
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// let sequelize;
+
+// if (process.env.DB_URL) {
+//   sequelize = new Sequelize(process.env.DB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: 'localhost',
+//       dialect: 'postgres'
+//     }
+//   );
+// }
+
+// module.exports = sequelize;
+
+const Sequelize = require("sequelize");
 require('dotenv').config();
 
-let sequelize;
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_NAME,
+  NODE_ENV = "production"
+} = process.env;
 
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'postgres'
+let db = 
+NODE_ENV === "production"
+  ? new Sequelize({
+      datbase: DB_NAME,
+      dialect: "postgres",
+      host: DB_HOST,
+      port: 5432,
+      username: DB_USER,
+      password: DB_PASSWORD,
+      pool: {
+        max: 3,
+        min: 1,
+        idle: 10000
+      }
+  })
+  : new Sequelize(
+    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+      logging: false, native: false
     }
-  );
-}
-
-module.exports = sequelize;
+  )
