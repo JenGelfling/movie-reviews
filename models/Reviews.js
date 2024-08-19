b02110457f0b45937b6f63128dfa6fa6d4d1c584
Ben Merchant <benmerch@gmail.com>
2024-08-18 21:31:53 -0500

    Added function to post a review. Still needs to actually verify user is logged in to do it though.

--- models/Reviews.js
+++ models/Reviews.js
@@ -29,7 +29,7 @@ Reviews.init(
     },
     like_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
         references: {
           model: 'likes',
           key: 'id'
