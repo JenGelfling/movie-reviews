3ed230e20bd4edb6cd74b72a21f6e153331830f1
Ben Merchant <benmerch@gmail.com>
2024-08-19 15:09:49 -0500

    Again.

--- models/Reviews.js
+++ models/Reviews.js
@@ -21,7 +21,7 @@ Reviews.init(
     },
     author_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
         references: {
           model: 'users',
           key: 'id'
