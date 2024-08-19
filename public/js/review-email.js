09bb95b490940ac77338645e3481e190ec90dc22
aiden0921an <nelsonaiden23@gmail.com>
2024-08-19 00:55:12 -0500

    nodemailer is done

--- public/js/review-email.js
+++ public/js/review-email.js
@@ -1,30 +1,30 @@
-// Import the Nodemailer library
-const nodemailer = require('nodemailer');
+// // Import the Nodemailer library
+// const nodemailer = require('nodemailer');
 
-// Create a transporter object
-const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false, // use false for STARTTLS; true for SSL on port 465
-  auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS,             
-  }
-});
+// // Create a transporter object
+// const transporter = nodemailer.createTransport({
+//   host: 'smtp.gmail.com',
+//   port: 587,
+//   secure: false, // use false for STARTTLS; true for SSL on port 465
+//   auth: {
+//     user: process.env.GMAIL_USER,
+//     pass: process.env.GMAIL_PASS,             
+//   }
+// });
 
-// Configure the mailoptions object
-const mailOptions = {
-  from: process.env.GMAIL_USER,
-  to: 'reviewergeneric90@gmail.com',
-  subject: 'Your review has been submitted successfully',
-  text: 'Your review has been posted!'
-};
+// // Configure the mailoptions object
+// const mailOptions = {
+//   from: process.env.GMAIL_USER,
+//   to: 'reviewergeneric90@gmail.com',
+//   subject: 'Your review has been submitted successfully',
+//   text: 'Your review has been posted!'
+// };
 
-// Send the email
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log('Error:', error);
-  } else {
-    console.log('Email sent: ', info.response);
-  }
-});
+// // Send the email
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log('Error:', error);
+//   } else {
+//     console.log('Email sent: ', info.response);
+//   }
+// });
